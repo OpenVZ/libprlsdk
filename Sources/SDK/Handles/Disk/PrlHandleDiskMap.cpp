0991ac434e6a1dab707807d451e089c8c32e8af8
Alexander Pisarev <apisarev@virtuozzo.com>
2016-02-20 18:47:16 +0300

    Restore PrlDiskMap_* functions in SDK. #PSBM-44453

---
+++ Sources/SDK/Handles/Disk/PrlHandleDiskMap.cpp
@@ -0,0 +1,47 @@
+/*
+ * PrlHandleDiskMap.cpp
+ *
+ * Copyright (C) 1999-2016 Parallels IP Holdings GmbH
+ *
+ * This file is part of Parallels SDK. Parallels SDK is free
+ * software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License,
+ * or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see
+ * <http://www.gnu.org/licenses/> or write to Free Software Foundation,
+ * 51 Franklin Street, Fifth Floor Boston, MA 02110, USA.
+ *
+ * Our contact details: Parallels IP Holdings GmbH, Vordergasse 59, 8200
+ * Schaffhausen, Switzerland; http://www.parallels.com/.
+ */
+
+#include "PrlHandleDiskMap.h"
+
+PrlHandleDiskMap::PrlHandleDiskMap(const QByteArray& bits_, PRL_UINT32 size_,
+		PRL_UINT32 granularity_): PrlHandleBase(PHT_VIRTUAL_DISK_MAP),
+		m_bits(bits_), m_size(size_), m_granularity(granularity_)
+{
+}
+
+PRL_RESULT PrlHandleDiskMap::getBits(PRL_VOID_PTR buffer_, PRL_UINT32& capacity_) const
+{
+	PRL_UINT32 u = capacity_;
+	capacity_ = m_bits.size();
+	if (NULL == buffer_)
+		return PRL_ERR_SUCCESS;
+
+	if (u < (PRL_UINT32)m_bits.size())
+		return PRL_ERR_BUFFER_OVERRUN;
+
+	memcpy(buffer_, m_bits.data(), m_bits.size());
+	return PRL_ERR_SUCCESS;
+}
+
---
+++ Sources/SDK/Handles/Disk/PrlHandleDiskMap.h
@@ -0,0 +1,62 @@
+/*
+ * PrlHandleDiskMap.h
+ *
+ * Copyright (C) 1999-2016 Parallels IP Holdings GmbH
+ *
+ * This file is part of Parallels SDK. Parallels SDK is free
+ * software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License,
+ * or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see
+ * <http://www.gnu.org/licenses/> or write to Free Software Foundation,
+ * 51 Franklin Street, Fifth Floor Boston, MA 02110, USA.
+ *
+ * Our contact details: Parallels IP Holdings GmbH, Vordergasse 59, 8200
+ * Schaffhausen, Switzerland; http://www.parallels.com/.
+ */
+
+#ifndef __PARALLELS_HANDLE_DISK_MAP_H__
+#define __PARALLELS_HANDLE_DISK_MAP_H__
+
+#include "PrlHandleBase.h"
+
+/**
+ * Handle object represents a map of altered blocks of a virtual disk.
+ */
+struct PrlHandleDiskMap: PrlHandleBase
+{
+	PrlHandleDiskMap(const QByteArray& bits_, PRL_UINT32 size_,
+		PRL_UINT32 granularity_);
+
+	PRL_UINT32 getSize() const
+	{
+		return m_size;
+	}
+
+	PRL_UINT32 getGranularity() const
+	{
+		return m_granularity;
+	}
+
+	PRL_RESULT getBits(PRL_VOID_PTR buffer_, PRL_UINT32& capacity_) const;
+
+private:
+	const QByteArray m_bits;
+	const PRL_UINT32 m_size;
+	const PRL_UINT32 m_granularity;
+};
+
+
+typedef PrlHandleSmartPtr<PrlHandleDiskMap> PrlHandleDiskMapPtr;
+
+
+#endif	// __PARALLELS_HANDLE_DISK_MAP_H__
+
