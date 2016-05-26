822cf7069c2550f23ea4584499d93288942f2009
Alexander Burluka <aburluka@virtuozzo.com>
2016-05-26 17:26:44 +0300

    Revert "Change default type of network adapter for win2k3 #PSBM-46305"
    This reverts commit 2e76ca77df6b4ae1b7c26c540a069f4b2b1dd90b.
    Fixes issue #PSBM-46306
    Signed-off-by: Alexander Burluka <aburluka@virtuozzo.com>

--- Sources/SDK/Handles/Vm/PrlHandleVmDefaultConfig.cpp
+++ Sources/SDK/Handles/Vm/PrlHandleVmDefaultConfig.cpp
@@ -1250,12 +1250,7 @@ bool PrlHandleVmDefaultConfig::AddDefaultNetwork ( CVmConfiguration& cfg, PRL_HA
 	SortDeviceListByIndex<CVmGenericNetworkAdapter >(cfg.getVmHardwareList()->m_lstNetworkAdapters);
 
 	unsigned int nVersion = cfg.getVmSettings()->getVmCommonOptions()->getOsVersion();
-	if (nVersion == PVS_GUEST_VER_WIN_2003)
-	{
-		// BSOD bug #PSBM-46305
-		network->setAdapterType(PNT_E1000);
-	}
-	else if (nVersion == 0 || PVS_GUEST_VIRTIO_SUPPORTED(nVersion))
+	if (nVersion == 0 || PVS_GUEST_VIRTIO_SUPPORTED(nVersion))
 	{
 		network->setAdapterType(PNT_VIRTIO);
 	}
