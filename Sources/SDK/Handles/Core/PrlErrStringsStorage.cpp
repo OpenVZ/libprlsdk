e8c44d59c32f3b3769dffc404a7dfd7e9be152fd
Igor Sukhih <igor@virtuozzo.com>
2017-04-27 14:34:58 +0300

    Update PRL_ERR_REMOTE_DISPLAY_WRONG_PORT_NUMBER message
    add allowed port range info
    
    Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

--- Sources/SDK/Handles/Core/PrlErrStringsStorage.cpp
+++ Sources/SDK/Handles/Core/PrlErrStringsStorage.cpp
@@ -1517,7 +1517,7 @@ void PrlErrStringsStorage::Initialize()
 	REGISTER_ERROR_STRING(
 		PRL_ERR_REMOTE_DISPLAY_WRONG_PORT_NUMBER,
 		tr("The port number is invalid."),
-		tr("The port number must be greater than 5899. Enter a valid number and try again."),
+		tr("The allowed Remote Display port number range is %1:%2. Enter a valid number and try again."),
 		false,
 		PMT_WARNING
 		);
